08a42762a2d42aaa4087d4e2506c46bc1f7efc89
Sonali <sonalimagar08@gmail.com>
2024-06-29 20:23:33 +0530

    This is first question

--- src/main/scala/question1.scala
+++ src/main/scala/question1.scala
@@ -1,12 +1,12 @@
 import org.apache.spark.SparkContext
 
-object first {
+object question1 {
   def main(args:Array[String]):Unit={
     val number=14
     if (number > 0 && number % 2 == 0) {
-      println("The number is possitive and even")
+      println("The number is positive and even")
     } else {
-      println("The number is not possitive and it is odd")
+      println("The number is not positive and it is odd")
     }
 
   }
