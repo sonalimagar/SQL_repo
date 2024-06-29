39558c773b46821fe9568dc5be79073aff28bb13
Sonali <sonalimagar08@gmail.com>
2024-06-29 19:16:25 +0530

    adding for example

---
+++ src/main/scala/demo.scala
@@ -0,0 +1,13 @@
+object demo {
+  def main(args: Array[String]): Unit ={
+  val number = 9
+
+  if (number % 2 == 0 || number % 3 == 0) {
+    println(s"$number is divisible by either 2 or 3")
+  }
+  else {
+    println(s"$number  is not divisible by either 2 or 3")
+  }
+}
+
+}
